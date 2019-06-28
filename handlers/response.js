12c81b94398aaa26e867da835e6e6b8f79195906
Juan Manuel <juan.15121001@gmail.com>
2019-06-28 14:32:36 -0500

    arreglando metodos de rutas, añadieno midlewares

--- handlers/response.js
+++ handlers/response.js
@@ -1,32 +1,43 @@
+'use strict'
+
 module.exports = (res) => {
     return {
-        resp404: function(message = "") {
+        resp404: function(message = '') {
             return res.status(404).json({
                 status: false,
                 message: 'Not Found: Recurso no encontrado ' + message
             })
         },
-        resp401: function(message = "") {
+        resp401: function(message = '') {
             return res.status(404).json({
                 status: false,
-                message: 'Unauthorized: Usuario incorrecto ' + message
+                message: 'Unauthorized: No tienes los permisos necesarios ' + message
             })
         },
-        resp200: function(item, message = "") {
+        resp403: function(err, req, res, next) {
+
+            if (err.code === 'permission_denied') {
+                res.status(403).json({
+                    status: false,
+                    message: 'Forbidden: No tiene los permisos necesarios'
+                });
+            }
+        },
+        resp200: function(item, message = '') {
             return res.status(200).json({
                 status: true,
                 item: item,
                 message: 'Ok: Operacion exitosa ' + message
             })
         },
-        resp201: function(item, message = "") {
+        resp201: function(item, message = '') {
             return res.status(201).json({
                 status: true,
                 item: item,
                 message: 'Ok: Recurso creado con exito ' + message
             })
         },
-        resp500: function(message = "") {
+        resp500: function(message = '') {
             return res.status(500).json({
                 status: false,
                 message: 'Internal Server Error: Ha ocurrido un error en el servidor ' + message
---
+++ wakaya-api/app/controllers/auth.controller.js
@@ -0,0 +1,105 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+const DB = require('../../db')
+const { error, response } = require('../../../handlers')
+
+const { auth } = require('../config')
+
+const errors = error()
+let message
+
+module.exports = async() => {
+    const { User } = await DB()
+
+    async function login(req, res, next) {
+        const body = req.body
+        let user
+        let userData
+
+        const resp = response(res)
+
+        try {
+            user = await User.findByEmailSelectPassword(body.email)
+        } catch (e) {
+            return resp.resp500()
+        }
+
+
+        if (!user) {
+            return resp.resp404(message = "El usuario no existe")
+        }
+
+        if (!bcrypt.compareSync(body.password, user.password)) {
+            return resp.resp404(message = "El password no existe")
+        }
+
+        user = await User.findById(user._id)
+
+        const payload = {
+            user: user,
+            permissions: []
+        }
+        if (user.type == 'client') {
+            payload.permissions = [
+                'user:read'
+            ]
+        } else if (user.type == 'producer') {
+            payload.permissions = [
+                'user:write',
+                'user:read'
+            ]
+        }
+
+
+        const token = jwt.sign(payload, auth.SEED, { expiresIn: 14400 }) // 4 horas
+
+        userData = {
+            user: user,
+            token: token
+        }
+
+        resp.resp200(userData)
+    }
+    async function register(req, res, next) {
+        const resp = response(res)
+        let body = req.body
+        let user
+
+        try {
+            user = await User.findByEmail(body.email)
+        } catch (e) {
+            return resp.resp500(message = "El usuario es invalido")
+        }
+
+        if (user) {
+            return resp.resp500(message = "El usuario ya existe")
+        }
+
+        if (!body.password || body.password == '') {
+            return resp.resp500(message = "El password es necesario")
+        }
+
+        body.password = bcrypt.hashSync(body.password, 10)
+
+        try {
+            user = await User.createOrUpdate(body)
+        } catch (e) {
+            return resp.resp500()
+        }
+
+
+        resp.resp201(user)
+    }
+
+
+    return {
+        login,
+        register
+    }
+}
+
+// implementar funcions
+// -->create
